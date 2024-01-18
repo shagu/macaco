a8e8f6e222f81ff54efefb9786403c3a825cbbe5
shagu <ericmauser@googlemail.com>
2024-01-18 22:40:00 +0100

    widgets: rewrite m-popover custom element

--- src/renderer/widgets/m-popover.js
+++ src/renderer/widgets/m-popover.js
@@ -2,8 +2,9 @@ import { html, css } from './m-template.js'
 
 export default class MPopover extends HTMLElement {
   static shadow = null
-  static background = null
-  static popovers = []
+
+  static popovers = {}
+  static buttons = {}
 
   static template = html`
     <slot></slot>
@@ -14,65 +15,47 @@ export default class MPopover extends HTMLElement {
       display: none;
       position: fixed;
 
-      width: min-content;
-      max-height: max-content;
+      top: 0;
+      left: 0;
 
-      background: var(--window-light);
-      border: 1px var(--border-normal) solid;
-      box-shadow: 0px 0px 5px var(--border-dark);
+      width: 100vw;
+      height: 100vh;
 
-      padding: 5px;
-      z-index: 16;
-    }
-
-    ::slotted(m-grid) {
-      margin: 2px;
-      padding: 4px;
-      background-color: var(--widget-normal);
-      border: 1px var(--border-normal) solid;
+      z-index: 64;
     }
   `
 
-  static backgroundStyle = `
-    display: none;
-    position: fixed;
-
-    top: 0;
-    left: 0;
-
-    width: 100vw;
-    height: 100vh;
-
-    z-index: 8;
-  `
-
-  visible = (state) => {
-    MPopover.background.style.display = state ? 'block' : 'none'
-    this.style.display = state ? 'block' : 'none'
-
-    this.buttons.forEach((button) => {
-      button.checked = !!state
-    })
-  }
-
-  toggle (button) {
-    if (this.style.display === 'block') {
-      this.visible(false)
-      return
+  toggle (menu, button) {
+    // update state of all buttons and menus
+    for (const [, popover] of Object.entries(MPopover.popovers)) {
+      if (popover === menu) {
+        popover.state = !popover.state
+      } else {
+        popover.state = false
+      }
     }
 
-    for (const popover of MPopover.popovers) { popover.visible(false) }
+    // reset all check buttons
+    for (const button of Object.entries(MPopover.buttons)) {
+      button.checked = false
+    }
 
-    this.anchor = button
-    this.visible(true)
-    this.position()
+    // check button and show menu
+    menu.style.display = menu.state ? 'block' : 'none'
+    button.checked = menu.state
+
+    // show clickable backdrop
+    this.style.display = menu.state ? 'block' : 'none'
+
+    // anchor menu to button
+    this.position(menu, button)
   }
 
-  position () {
-    if (this.anchor) {
+  position (menu, button) {
+    if (button) {
       // move popover to last anchor
-      const toggleRect = this.anchor.getBoundingClientRect()
-      const popoverRect = this.getBoundingClientRect()
+      const toggleRect = button.getBoundingClientRect()
+      const popoverRect = menu.getBoundingClientRect()
 
       const horizontal = (toggleRect.left + toggleRect.right) / 2 < window.innerWidth / 2 ? 'left' : 'right'
       const vertical = (toggleRect.top + toggleRect.bottom) / 2 < window.innerHeight / 2 ? 'top' : 'bottom'
@@ -80,46 +63,88 @@ export default class MPopover extends HTMLElement {
       const left = horizontal === 'left' ? toggleRect.left : toggleRect.left + toggleRect.width - popoverRect.width
       const top = vertical === 'top' ? toggleRect.top + toggleRect.height : toggleRect.top - popoverRect.height
 
-      this.style.top = `${top}px`
-      this.style.left = `${left}px`
-    } else {
+      menu.style.top = `${top}px`
+      menu.style.left = `${left}px`
+    } else if (menu) {
       // center popover on screen
-      const popoverRect = this.getBoundingClientRect()
+      const popoverRect = menu.getBoundingClientRect()
       const top = window.innerHeight / 2 - popoverRect.height / 2
       const left = window.innerWidth / 2 - popoverRect.width / 2
 
-      this.style.top = `${top}px`
-      this.style.left = `${left}px`
+      menu.style.top = `${top}px`
+      menu.style.left = `${left}px`
     }
+  }
+
+  connectedCallback () {
+    const slots = this.shadow.querySelector('slot')
+    const popovers = slots.assignedElements({ flatten: true })
+
+    for (const button of this.querySelectorAllShadows('[m-popover]')) {
+      const label = button.getAttribute('m-popover')
+      MPopover.buttons[label] = button
+    }
+
+    for (const popover of popovers) {
+      const label = popover.getAttribute('id')
+      MPopover.popovers[label] = popover
+    }
+
+    for (const [id, button] of Object.entries(MPopover.buttons)) {
+      if (MPopover.popovers[id]) {
+        button.addEventListener('click', (ev) => {
+          button.style.position = button.style.position || 'relative'
+          button.style.zIndex = '32'
+          this.toggle(MPopover.popovers[id], button)
+        })
+      }
+    }
+  }
+
+  querySelectorAllShadows (selector, el = document.body) {
+    // credits to @domiii <https://stackoverflow.com/a/71692555>
+    // recurse on childShadows
+    const childShadows = Array.from(el.querySelectorAll('*')).map(el => el.shadowRoot).filter(Boolean)
+    const childResults = childShadows.map(child => this.querySelectorAllShadows(selector, child))
+
+    // fuse all results into singular, flat array
+    const result = Array.from(el.querySelectorAll(selector))
+    return result.concat(childResults).flat()
   }
 
   constructor () {
     super()
 
     this.shadow = this.attachShadow({ mode: 'open' })
-    this.shadow.adoptedStyleSheets = [MPopover.style]
-    this.shadow.append(document.importNode(MPopover.template, true))
-    MPopover.popovers.push(this)
+    this.shadow.adoptedStyleSheets = [this.constructor.style]
+    this.shadow.append(document.importNode(this.constructor.template, true))
 
     // reload position on resize
-    window.addEventListener('resize', () => { this.position() })
-
-    // attach popover to button
-    this.buttons = document.querySelectorAll(`[m-popover="${this.getAttribute('name')}"]`)
-    this.buttons.forEach((button) => {
-      button.addEventListener('click', (ev) => this.toggle(button))
-      button.style.position = button.style.position || 'relative'
-      button.style.zIndex = '32'
+    window.addEventListener('resize', () => {
+      for (const [id, popover] of Object.entries(MPopover.popovers)) {
+        if (popover.state === true) {
+          this.position(popover, MPopover.buttons[id])
+        }
+      }
     })
 
     // create clickable background
-    if (MPopover.background) return
-    MPopover.background = document.createElement('div')
-    MPopover.background.style.cssText = MPopover.backgroundStyle
-    document.body.appendChild(MPopover.background)
+    this.addEventListener('click', (ev) => {
+      // reset all check buttons
+      for (const [, button] of Object.entries(MPopover.buttons)) {
+        button.checked = false
+      }
 
-    MPopover.background.addEventListener('click', (ev) => {
-      for (const popover of MPopover.popovers) { popover.visible(false) }
+      // update state of all buttons and menus
+      for (const [, popover] of Object.entries(MPopover.popovers)) {
+        popover.style.display = 'none'
+        popover.state = false
+      }
+
+      // show clickable backdrop
+      this.style.display = 'none'
+
+      // stop propagation
       ev.stopPropagation()
     })
   }
